90ccd46c5683911fbc0c4f7735af4f85bd193b56
Florian Schuster <mail@florianschuster.at>
2024-10-25 10:59:00 +0200

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "at.florianschuster.hydro"
         minSdk = 31
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         vectorDrawables {
